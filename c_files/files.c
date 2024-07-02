7da811b992eb84717ebb6bf7c582518f40e6af8a
Billy <coffinw9@gmail.com>
2024-07-02 15:58:45 -0400

    kept it pg

--- c_files/files.c
+++ c_files/files.c
@@ -7,8 +7,11 @@ int main(void)
     FILE *fp;
     // Variable to represent open file
     fp = fopen("hello.txt", "r"); // Open file for reading
-    int c = fgetc(fp);
-    printf("%c\n", c); // Read a single character
+    int c;
+
+    // loop until we hit EOF
+    while ((c = fgetc(fp)) != EOF)
+        printf("%c", c); // Read a single character
     // Print char to stdout
     fclose(fp); // Close the file when done
 }
